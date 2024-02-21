18428794873255ef7d686e2236732158ef8b82df
Éleson Silva <elesonsilva9@gmail.com>
2024-02-21 11:45:13 -0300

    criando pagina inicial

--- src/app/components/pages/jogos/street-figther/street-figther.component.ts
+++ src/app/components/pages/jogos/street-figther/street-figther.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Jogos, Personagem } from '../../../../models/jogos.models';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-street-figther',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './street-figther.component.html',
   styleUrl: './street-figther.component.scss'
 })
 export class StreetFigtherComponent {
+personagens : Personagem[]=[];
 
+  constructor (private httpclient:HttpClient){}
+
+   ngOnInit(): void{
+    this.httpclient.get<{ Jogos: Jogos[]}>('assets/dados/dados.json').subscribe(data=>{
+      this.personagens = data.Jogos[2].personagens
+    });
+  }
 }
